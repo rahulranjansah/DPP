86ad9ed8cef6cc8702ddb47468d03590cec418f7
Rahul Ranjan Sah <rahulranjann45@gmail.com>
2024-05-22 21:18:51 -0400

    2-d problems

--- 21.py
+++ 21.py
@@ -6,19 +6,21 @@ class ListNode:
 
 
 class Solution:
+
     def mergeTwoLists(self, list1, list2):
-        dummy = ListNode()
-        tail = dummy
+        dummy = ListNode() # this acts as a placeholder and initalize the ListNode() with dummy where val = 0 and next=None we have no link 
+        # dummy = ListNode is similar to dummy = []
+        tail = dummy # this initalize dummy node with val = 0 and next = head of the element both has same point start
 
         while list1 and list2:
             if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
+                tail.next = list1  # appends node analogus
+                list1 = list1.next # moves the pointer node analogus given this is a listnode
             else:
                 tail.next = list2
                 list2 = list2.next
 
-            tail = tail.next
+            tail = tail.next # when list1.val = list2.val? no
         if list1:
             tail.next = list1
         else:
@@ -27,7 +29,6 @@ class Solution:
         return dummy.next
 
 
-list1 = [1,2,4]
-list2 = [2,3,4]
+
 solution = Solution()
 print(solution.mergeTwoLists(list1, list2))
